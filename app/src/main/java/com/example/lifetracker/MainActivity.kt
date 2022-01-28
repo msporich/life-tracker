17639c0593367e6147ee2f02e0faa645578ba2e2
msporich <marksporich@gmail.com>
2022-01-27 21:39:54 -0500

    added full registration and login functionality

--- app/src/main/java/com/example/lifetracker/MainActivity.kt
+++ app/src/main/java/com/example/lifetracker/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.lifetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
     }
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+
+            var intent = Intent(this, ModulesActivity::class.java)
+            intent.putExtra("user", user)
+            startActivity(intent)
+
+        } else {
+            Toast.makeText(this, "Log In Failed", Toast.LENGTH_LONG).show()
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
