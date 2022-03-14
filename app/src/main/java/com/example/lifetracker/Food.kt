cb5bf90abb7d3cc49266d98994cf562c3a757181
Jakob <200400481@student.georgianc.on.ca>
2022-03-13 21:53:12 -0400

    Create Food model

---
+++ app/src/main/java/com/example/lifetracker/Food.kt
@@ -0,0 +1,10 @@
+package com.example.lifetracker
+
+import java.util.*
+
+class Food (var id: String?=null,
+            var ownerId: String?=null,
+            var foodName: String?=null,
+            var foodCalories: Int?=null,
+            var foodCategory: String?=null,
+            var dateConsumed: Date?=null)
