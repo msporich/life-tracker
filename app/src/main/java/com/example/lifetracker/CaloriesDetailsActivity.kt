8a29481a092982a2b2afb507ce4396a69cea5046
Jakob <200400481@student.georgianc.on.ca>
2022-04-16 19:05:44 -0400

    Implement delete calorie functionality

--- app/src/main/java/com/example/lifetracker/CaloriesDetailsActivity.kt
+++ app/src/main/java/com/example/lifetracker/CaloriesDetailsActivity.kt
@@ -1,8 +1,12 @@
 package com.example.lifetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import com.example.lifetracker.databinding.ActivityCaloriesDetailsBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CaloriesDetailsActivity : AppCompatActivity() {
 
@@ -24,7 +28,35 @@ class CaloriesDetailsActivity : AppCompatActivity() {
         binding.textViewCategory.text = intent.getStringExtra("foodCategory")
         binding.textViewDateConsumed.text = dateConsumedFormatted
 
+        // Delete the calorie data if delete button is clicked then redirect to main schedule
+        binding.buttonDelete.setOnClickListener{
 
+            val builder = AlertDialog.Builder(this)
+            builder
+                .setMessage("Delete forever?")
+                .setCancelable(true)
+                .setPositiveButton("Delete") { dialog, id ->
+                    // DELETE
+                    val db = FirebaseFirestore.getInstance()
+                    db
+                        .collection("food")
+                        .document(intent.getStringExtra("foodId").toString())
+                        .delete()
+                        .addOnSuccessListener { Log.i("Database", "Calorie record deleted successfully") }
+                        .addOnFailureListener { e -> Log.w("Database", "Error deleting document", e) }
+                    Log.i("Database", "Calorie entry deleted")
+
+                    // Redirect to main schedule and display toast
+                    val intent = Intent(this, CaloriesRecyclerActivity::class.java)
+                    intent.putExtra("toast", "Calorie entry deleted")
+                    startActivity(intent)
+                }
+                .setNegativeButton("Cancel") { dialog, id ->
+                    Log.i("Database", "Action was cancelled")
+                }
+            val alert = builder.create()
+            alert.show()
+        }
 
     }
 }
